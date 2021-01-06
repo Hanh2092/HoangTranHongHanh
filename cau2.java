5bf7cd7511635df93efe4fa8d0d4a920dcaad154
HoangTranHongHanh <76670859+HoangTranHongHanh@users.noreply.github.com>
2021-01-06 07:47:40 +0700

    Add files via upload

---
+++ cau2.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class cau2 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("nhập số thứ nhất: ");
+        int a = s.nextInt();
+        System.out.print("nhập số thứ hai: ");
+        int b = s.nextInt();
+        System.out.print("nhập số thứ ba: ");
+        int c = s.nextInt();
+        if (a > 2 && b > 2 && c > 2) {
+            if (a > b && a > c) {
+                System.out.print("Số lớn nhất trong 3 số là: " + a);
+            }
+            if (b > a && b > c) {
+                System.out.print("Số lớn nhất trong 3 số là: " + b);
+            }
+            if (c > b && c > a) {
+                System.out.print("Số lớn nhất trong 3 số là: " + c);
+            }
+        } else {
+            System.out.print("Yêu cầu cả 3 số phải đều phải lớn hơn 2.");
+        }
+    }
+}
---
+++ cau5.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class cau5 {
+    boolean KTSNT(int n){
+        for(int i =2; i<=Math.sqrt(n);i++){
+            if(n%i == 0){
+                return false;
+            }
+        }
+        return true;
+    }
+    public static void main(String args[]){
+        Scanner s = new Scanner(System.in);
+        System.out.print("Nhập số cần kiểm tra: ");
+        int n = s.nextInt();
+        cau5 snt = new cau5();
+        if(snt.KTSNT(n)){
+            System.out.print("Số: " +n +" là số nguyên tố.");
+        }else{
+            System.out.print("Số: " +n +" không phải là số nguyên tố.");
+        }
+    }
+}
